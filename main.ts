ed946979571ad4376bc797b47c4084a8d16a7baf
bkp18 <43169378+bkp18@users.noreply.github.com>
2020-09-18 02:04:25 +0200

    Update pxt.json, main.blocks, main.ts, fireflytestv3.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+basic.showLeds(`
+    # . # . #
+    # . # . #
+    . # . # .
+    # . # . #
+    # . # . #
+    `)
 basic.forever(function () {
-	
+    Firefly.setServo(Firefly.ServoPinValues.servo1, 0)
+    basic.pause(2000)
+    Firefly.setServo(Firefly.ServoPinValues.servo1, 180)
+    basic.pause(2000)
+    Firefly.setDC(Firefly.DCPinValues.dcmotor4, DCDirectionValuesDansk.forward, 25)
+    basic.pause(1000)
+    Firefly.setDC(Firefly.DCPinValues.dcmotor4, DCDirectionValuesDansk.forward, 0)
+    basic.pause(1000)
 })
